6f87e9c0779cc01ee39990c4da58231e4ea94ead
AncientTangeloes <119451157+AncientTangeloes@users.noreply.github.com>
2024-05-02 08:26:56 -0500

    rudimentary filter system

--- CIS 296 Final Project/Controllers/HomeController.cs
+++ CIS 296 Final Project/Controllers/HomeController.cs
@@ -42,5 +42,149 @@ namespace CharacterList.Controllers
 
             return View(characters);
         }
+
+        public IActionResult Anime(int id)
+        {
+            var characters = context.Characters.Include(c => c.OgType).Where(c => c.OgType.OgTypeId == "A").ToList();
+
+            if (id == 1)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Name).Where(c => c.OgType.OgTypeId == "A").ToList();
+            }
+            else if (id == 2)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Origin).Where(c => c.OgType.OgTypeId == "A").ToList();
+            }
+            else if (id == 3)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Year).Where(c => c.OgType.OgTypeId == "A").ToList();
+            }
+            else
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.CharacterId).Where(c => c.OgType.OgTypeId == "A").ToList();
+            }
+
+            return View(characters);
+        }
+
+        public IActionResult Comic(int id)
+        {
+            var characters = context.Characters.Include(c => c.OgType).Where(c => c.OgType.OgTypeId == "C").ToList();
+
+            if (id == 1)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Name).Where(c => c.OgType.OgTypeId == "C").ToList();
+            }
+            else if (id == 2)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Origin).Where(c => c.OgType.OgTypeId == "C").ToList();
+            }
+            else if (id == 3)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Year).Where(c => c.OgType.OgTypeId == "C").ToList();
+            }
+            else
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.CharacterId).Where(c => c.OgType.OgTypeId == "C").ToList();
+            }
+
+            return View(characters);
+        }
+
+        public IActionResult Movie(int id)
+        {
+            var characters = context.Characters.Include(c => c.OgType).Where(c => c.OgType.OgTypeId == "F").ToList();
+
+            if (id == 1)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Name).Where(c => c.OgType.OgTypeId == "F").ToList();
+            }
+            else if (id == 2)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Origin).Where(c => c.OgType.OgTypeId == "F").ToList();
+            }
+            else if (id == 3)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Year).Where(c => c.OgType.OgTypeId == "F").ToList();
+            }
+            else
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.CharacterId).Where(c => c.OgType.OgTypeId == "F").ToList();
+            }
+
+            return View(characters);
+        }
+
+        public IActionResult Game(int id)
+        {
+            var characters = context.Characters.Include(c => c.OgType).Where(c => c.OgType.OgTypeId == "G").ToList();
+
+            if (id == 1)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Name).Where(c => c.OgType.OgTypeId == "G").ToList();
+            }
+            else if (id == 2)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Origin).Where(c => c.OgType.OgTypeId == "G").ToList();
+            }
+            else if (id == 3)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Year).Where(c => c.OgType.OgTypeId == "G").ToList();
+            }
+            else
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.CharacterId).Where(c => c.OgType.OgTypeId == "G").ToList();
+            }
+
+            return View(characters);
+        }
+
+        public IActionResult Manga(int id)
+        {
+            var characters = context.Characters.Include(c => c.OgType).Where(c => c.OgType.OgTypeId == "M").ToList();
+
+            if (id == 1)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Name).Where(c => c.OgType.OgTypeId == "M").ToList();
+            }
+            else if (id == 2)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Origin).Where(c => c.OgType.OgTypeId == "M").ToList();
+            }
+            else if (id == 3)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Year).Where(c => c.OgType.OgTypeId == "M").ToList();
+            }
+            else
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.CharacterId).Where(c => c.OgType.OgTypeId == "M").ToList();
+            }
+
+            return View(characters);
+        }
+
+        public IActionResult Cartoon(int id)
+        {
+            var characters = context.Characters.Include(c => c.OgType).Where(c => c.OgType.OgTypeId == "S").ToList();
+
+            if (id == 1)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Name).Where(c => c.OgType.OgTypeId == "S").ToList();
+            }
+            else if (id == 2)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Origin).Where(c => c.OgType.OgTypeId == "S").ToList();
+            }
+            else if (id == 3)
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.Year).Where(c => c.OgType.OgTypeId == "S").ToList();
+            }
+            else
+            {
+                characters = context.Characters.Include(c => c.OgType).OrderBy(c => c.CharacterId).Where(c => c.OgType.OgTypeId == "S").ToList();
+            }
+
+            return View(characters);
+        }
     }
 }
